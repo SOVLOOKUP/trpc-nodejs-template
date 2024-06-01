8626af1de16b80eba965b782d6646c54c44ca80e
sovlookup <gonorth@qq.com>
2024-06-02 01:38:01 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import { Hono } from 'hono'
+import { trpcServer } from "@hono/trpc-server";
+import { getWSConnectionHandler } from '@trpc/server/adapters/ws';
+import { Hono } from "hono";
+import { appRouter } from "./routes";
+import { cors } from "hono/cors";
+import { createContext } from "./context";
+import { upgradeWebSocket } from "./ws";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  "/trpc/*",
+  cors(),
+);
+
+app.use(
+  "/trpc",
+  upgradeWebSocket(getWSConnectionHandler({
+    router: appRouter,
+    createContext
+  })),
+);
+
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext
+  }),
+);
 
 export default app
